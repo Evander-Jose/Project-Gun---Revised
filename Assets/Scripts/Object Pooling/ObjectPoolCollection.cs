98b5a6fef750332880346e5197618af395cd705d
Evander-Jose <jemandexistiert@gmail.com>
2021-04-03 19:46:37 +0700

    Gun Tracers and Animations Added
    Gun tracers and animations play when the player fires a weapon.

---
+++ Assets/Scripts/Object Pooling/ObjectPoolCollection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolCollection : MonoBehaviour
+{
+    //This design of object pooling is rather easy to use, but:
+    //ony use this, to activate and deactivate objects from a group, where
+    //which object that is being activated doesn't matter,
+    //the size of pooled objects is unlikely to change.
+    private Queue<GameObject> deactivatedObjects = new Queue<GameObject>();
+    private Queue<GameObject> activatedObjects = new Queue<GameObject>();
+    public void RegisterPooledObject(GameObject objectToAdd)
+    {
+        deactivatedObjects.Enqueue(objectToAdd);
+        objectToAdd.SetActive(false);
+    }
+
+    public GameObject ActivateObject()
+    {
+        GameObject activatedObject = deactivatedObjects.Dequeue();
+
+        activatedObject.SetActive(true);
+
+        activatedObjects.Enqueue(activatedObject);
+        return activatedObject;
+    }
+    public GameObject DeactivateObject()
+    {
+        GameObject deactivatedObject = activatedObjects.Dequeue();
+
+        deactivatedObject.SetActive(false);
+
+        deactivatedObjects.Enqueue(deactivatedObject);
+
+        return deactivatedObject;
+    }
+}
--- Assets/Scripts/Player Weapons System/Gun.cs
+++ Assets/Scripts/Player Weapons System/Gun.cs
@@ -8,9 +8,32 @@ public class Gun : Weapon
     [Space]
     public Transform firstPersonCamera;
     public LayerMask targetLayerMask;
+    [Space]
+    [Header("Tracer Particle Effect Settings")]
+    public ObjectPoolCollection objectPool;
+    public GameObject tracerParticle;
+    public int tracerParticlePoolSize = 0;
+    [Space]
+    public float tracerTravelSpeed = 30f;
+    public float tracerLifetime = 0.7f;
+    [Space]
+    public Transform muzzle;
+    public Animator anim;
+
 
     private float timeSinceLastFire = 0f;
     private bool canFire = true;
+
+    private void Awake()
+    {
+        //Registers some tracer particles to the object pool asset.
+        for(int i = 0; i < tracerParticlePoolSize; i++)
+        {
+            GameObject newTracer = Instantiate(tracerParticle, objectPool.transform, false);
+            newTracer.transform.position = Vector3.zero;
+            objectPool.RegisterPooledObject(newTracer);
+        }
+    }
 
     public override GameObject GetTarget()
     {
@@ -21,7 +44,16 @@ public class Gun : Weapon
         RaycastHit rayHit;
         Ray gunRay = new Ray(transform.position, firstPersonCamera.transform.forward);
 
-        Debug.DrawRay(transform.position, firstPersonCamera.transform.forward * 10f, Color.red, 5f);
+        //Debug.DrawRay(transform.position, firstPersonCamera.transform.forward * 10f, Color.red, 5f);
+        //Activate a tracer gameobject and then give it velocity:
+        GameObject activatedTracer = objectPool.ActivateObject();
+        activatedTracer.transform.position = muzzle.position;
+        Debug.Log("Tracer fired from " + activatedTracer.transform.localPosition);
+        activatedTracer.GetComponent<Rigidbody>().velocity = firstPersonCamera.forward * tracerTravelSpeed;
+
+        Invoke("TurnOffTracer", tracerLifetime);
+
+        anim.SetTrigger("shoot");
 
         if (Physics.Raycast(gunRay, out rayHit, gunSetting.range, targetLayerMask))
         {
@@ -30,6 +62,11 @@ public class Gun : Weapon
         {
             return null;
         }
+    }
+
+    private void TurnOffTracer()
+    {
+        objectPool.DeactivateObject();
     }
 
     public override void InflictDamageToTarget(Health other)
