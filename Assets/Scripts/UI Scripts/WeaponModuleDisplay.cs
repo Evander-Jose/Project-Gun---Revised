4b990f41fa1e2153a514d51e654f1f9e940155cd
Evander-Jose <jemandexistiert@gmail.com>
2021-04-13 12:24:00 +0700

    Better modules display
    Instead of instantiating labels, it just changes the text of the labels that were already created.

--- Assets/Scripts/UI Scripts/WeaponModuleDisplay.cs
+++ Assets/Scripts/UI Scripts/WeaponModuleDisplay.cs
@@ -6,26 +6,21 @@ using TMPro;
 public class WeaponModuleDisplay : MonoBehaviour
 {
     public GameObject moduleTextPrefab;
+    public TextMeshProUGUI[] moduleLabels;
+    private List<WeaponModule> weaponModules = new List<WeaponModule>();
 
     private void Update()
     {
-        
-
         StartCoroutine(DisplayModules(PlayerWeaponControls.instance.playerWeaponUser.activeWeapon.weaponModules));
-        
     }
 
     private IEnumerator DisplayModules(List<WeaponModule> listOfModules)
     {
-
-        for (int i = 0; i < listOfModules.Count; i++)
+        for (int i = 0; i < moduleLabels.Length; i++)
         {
-            //Create a new module text prefab:
-            GameObject newText = Instantiate(moduleTextPrefab, transform, false);
-            TextMeshProUGUI text = newText.GetComponent<TextMeshProUGUI>();
-            text.text = listOfModules[i].name;
-            Destroy(newText, Time.fixedDeltaTime);
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
+            if(moduleLabels[i] != null)
+                moduleLabels[i].text = listOfModules[i].name;
+            yield return new WaitForSeconds(0.05f);
         }
     }
 }
