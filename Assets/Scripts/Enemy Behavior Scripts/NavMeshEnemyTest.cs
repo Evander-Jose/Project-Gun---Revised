3f677e800388265c885102aed6c5272a596645fe
Evander-Jose <jemandexistiert@gmail.com>
2021-04-24 14:45:34 +0700

    First Steps Toward AI Enemies
    So, I made a test script that just follows the player around. The enemies can now avoid obstacles, climb up stairs, and climb up slopes, all with the help of the Unity NavMesh system.
    
    Well, it's not much, but it's a first step towards proper NavMesh enemies.

---
+++ Assets/Scripts/Enemy Behavior Scripts/NavMeshEnemyTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshEnemyTest : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    private PlayerWeaponControls playerObject;
+
+    private void Start()
+    {
+        playerObject = FindObjectOfType<PlayerWeaponControls>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(playerObject.transform.position);
+    }
+}
