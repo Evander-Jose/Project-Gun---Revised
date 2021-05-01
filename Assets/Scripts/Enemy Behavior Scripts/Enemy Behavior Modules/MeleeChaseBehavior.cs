d06a3731f7119decd2d973789b1c8abda12bed64
Evander-Jose <jemandexistiert@gmail.com>
2021-05-01 13:57:16 +0700

    Rooms have their own NavMeshSurfaces
    With the help of the NavMeshSurface script that I downloaded from the official Unity repository on GitHub, I was able to make each room have their own NavMeshSurface unique to their own pre-designed layout.

---
+++ Assets/Scripts/Enemy Behavior Scripts/Enemy Behavior Modules/MeleeChaseBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(EnemyBaseBehavior))]
+[RequireComponent(typeof(Rigidbody))]
+public class MeleeChaseBehavior : MonoBehaviour
+{
+    private EnemyBaseBehavior baseBehavior;
+
+    public NavMeshAgent agent;
+    public float movementSpeed;
+    [Space]
+    public float attackDelay;
+    public float damageAmount;
+    private Rigidbody rb;
+
+    private float timeSinceLastAttack = 0f;
+
+    private Health playerHealthComponent;
+
+    private void Start()
+    {
+        baseBehavior = GetComponent<EnemyBaseBehavior>();
+        baseBehavior.behaviorWhileInRange += AttackPlayer;
+        baseBehavior.behaviorWhilePlayerOutOfRange += ChasePlayer;
+
+        rb = GetComponent<Rigidbody>();
+        playerHealthComponent = baseBehavior.PlayerTransform.GetComponent<Health>();
+    }
+
+    private void ChasePlayer()
+    {
+        if (agent.SetDestination(baseBehavior.PlayerTransform.position))
+        {
+            rb.velocity = agent.desiredVelocity * movementSpeed * Time.deltaTime;
+        }
+
+        timeSinceLastAttack += Time.deltaTime;
+    }
+
+    private void AttackPlayer()
+    {
+        if (timeSinceLastAttack > attackDelay)
+        {
+            timeSinceLastAttack = 0f;
+            playerHealthComponent.DealDamage(damageAmount);
+        }
+        else
+        {
+            timeSinceLastAttack += Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy Behavior Scripts/EnemyBaseBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBaseBehavior : MonoBehaviour
+{
+    //This is a script that all enemies in the game will have.
+
+    //The main function of this script is to just execute delegates/events within state machines.
+
+    //Initializing the delegates:
+
+    //Maybe, when I have the time, I would implement much more complex behavior to the enemies.
+
+    //public delegate void WhileHealthIsLow();
+    //public event WhileHealthIsLow behaviorWhileHealthIsLow;
+
+    public delegate void WhilePlayer_OutOfRange();
+    public event WhilePlayer_OutOfRange behaviorWhilePlayerOutOfRange;
+
+    public delegate void WhilePlayer_InRange();
+    public event WhilePlayer_InRange behaviorWhileInRange;
+
+    public enum EnemyState {OutsideRange, InsideRange}
+    public EnemyState currentState;
+
+    public float detectionDistance = 5f;
+
+    private Transform playerTransform;
+    public Transform PlayerTransform { get { return playerTransform;  } }
+
+    private void Awake()
+    {
+        playerTransform = FindObjectOfType<PlayerWeaponControls>().transform;
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case EnemyState.OutsideRange:
+                {
+                    behaviorWhilePlayerOutOfRange?.Invoke();
+                    break;
+                }
+
+            case EnemyState.InsideRange:
+                {
+                    behaviorWhileInRange?.Invoke();
+                    break;
+                }
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float distanceFromPlayer = Vector3.Distance(playerTransform.position, transform.position);
+        
+        if(distanceFromPlayer >= detectionDistance)
+        {
+            currentState = EnemyState.InsideRange;
+        } else
+        {
+            currentState = EnemyState.OutsideRange;
+        }
+    }
+}
