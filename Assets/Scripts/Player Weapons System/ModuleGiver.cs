58bee36b0296eea1f473a3ff44e01db9a5524df8
Evander-Jose <jemandexistiert@gmail.com>
2021-05-13 15:38:34 +0700

    Module activation added
    So I made the 'WeaponType' scriptable object, which just contains a list of 'WeaponComponentType' scriptable objects, which would be compatible with that type of weapon.
    
    Using this, I made a monobehavior called a WeaponComponentActivator, which as its name suggests, activates components, but only if a method inside of it is called by some other script from outside the gameobject. This is where the 'ModuleGiver' comes in.
    
    Yeah, yeah, I know that 'WeaponModules' and 'WeaponComponents' have different names, but I still see them as the same thing.

---
+++ Assets/Scripts/Player Weapons System/ModuleGiver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleGiver : MonoBehaviour
+{
+    public void ActivateModuleInActiveWeapon(WeaponComponentType weaponComponentType)
+    {
+        Debug.Log("Activated Module: " + weaponComponentType.name);
+        WeaponComponentActivator.activeInstance.ActivateWeaponComponent(weaponComponentType);
+    }
+}
---
+++ Assets/Scripts/Player Weapons System/Weapon Component/WeaponType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapon Type")]
+public class WeaponType : ScriptableObject
+{
+    public WeaponComponentType[] compatibleWeaponComponentTypes;
+    public bool CheckCompatibility(WeaponComponentType type)
+    {
+        foreach(WeaponComponentType wct in compatibleWeaponComponentTypes)
+        {
+            if (wct.name == type.name)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
