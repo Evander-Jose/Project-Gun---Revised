13f6232a03afb445d79533df86b7752a87701d82
Evander-Jose <jemandexistiert@gmail.com>
2021-05-17 10:26:07 +0700

    Level List Added
    A level list is a scriptable object that stores an array of build indexes (integers), and then all game objects could just get a reference to that scriptable object, and load the next level in the list.
    
    This can be useful for making level packs.

---
+++ Assets/Scripts/Level Loading/LevelList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Level List")]
+public class LevelList : ScriptableObject
+{
+    public int[] sceneBuildIndexes;
+    public int currentScene = 0;
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(sceneBuildIndexes[currentScene]);
+    }
+
+    public void LoadNextLevel()
+    {
+        currentScene++;
+        if (currentScene > sceneBuildIndexes.Length - 1)
+            currentScene = sceneBuildIndexes.Length - 1;
+
+        SceneManager.LoadSceneAsync(sceneBuildIndexes[currentScene]);
+    }
+}
---
+++ Assets/TestTeleportToEnd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTeleportToEnd : MonoBehaviour
+{
+    public Vector3 endCoordinates;
+    public CharacterController playerController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            playerController.transform.position = endCoordinates;
+        }
+    }
+}
