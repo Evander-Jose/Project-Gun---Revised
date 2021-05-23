4258eaccf434371ec3d9dbca3de58f9bebced49f
Evander-Jose <jemandexistiert@gmail.com>
2021-05-23 20:33:28 +0700

    Health Bar Added
    Just a little health bar added. This feature was long overdue.
    
    Oh, and to make things easier for future stuff, I made a little script that would read the current health and max health values in the health component in the player, then put those values into scriptable object variables.

---
+++ Assets/Scripts/UI Scripts/HealthBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public FloatVariable playerHealthVariable;
+    public FloatVariable maxPlayerHealthVariable;
+    public Slider healthBarSlider;
+
+    private void Update()
+    {
+        healthBarSlider.value = playerHealthVariable.Value / maxPlayerHealthVariable.Value;
+    }
+}
---
+++ Assets/Scripts/UI Scripts/ReadPlayerHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadPlayerHealth : MonoBehaviour
+{
+    public Health health;
+    public FloatVariable playerHealthVariable;
+    public FloatVariable maxPlayerHealthVariable;
+    private void Update()
+    {
+        playerHealthVariable.Value = health.currentHealth;
+        maxPlayerHealthVariable.Value = health.maxHealth;
+    }
+}
