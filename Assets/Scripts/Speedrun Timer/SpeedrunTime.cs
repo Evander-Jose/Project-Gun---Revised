3a023413418c79bd0a369af7feef23c692e0f902
Evander-Jose <jemandexistiert@gmail.com>
2021-05-26 13:45:03 +0700

    Speedrun Time Struct Added
    A speedrun time struct has been added to make things a bit easier.
    
    Easier for the speedrun clock to read the time.

---
+++ Assets/Scripts/Speedrun Timer/SpeedrunTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public struct SpeedrunTime
+{
+    public int minutes;
+    public int seconds;
+    public int milliSeconds;
+
+    public SpeedrunTime(float totalSeconds)
+    {
+        //Calculate milliSeconds:
+        float milliSecondsDecimal = totalSeconds - (float)Math.Truncate(totalSeconds);
+        this.milliSeconds = (int)Math.Round(milliSecondsDecimal * 100f);
+
+        //Calculate seconds:
+        int minutesQuotient = (int)Math.Truncate(totalSeconds / 60f);
+        this.seconds = (int)Math.Truncate(totalSeconds) - 60 * minutesQuotient;
+
+        //Calculate minutes:
+        this.minutes = (int)Math.Truncate(totalSeconds/60f);
+    }
+}
--- Assets/Scripts/UI Scripts/SpeedrunTimerDisplay.cs
+++ Assets/Scripts/UI Scripts/SpeedrunTimerDisplay.cs
@@ -11,50 +11,7 @@ public class SpeedrunTimerDisplay : MonoBehaviour
 
     private void Update()
     {
-        //This timer will display milliseconds, seconds and minutes.
-
-        #region Millis
-        //To calculate the milliseconds, I would just take the speedrun seconds and then subtract them by the nearest whole number:
-        float millis = speedrunSeconds.Value - Mathf.Round(speedrunSeconds.Value);
-
-        //If the seconds got rounded up, then millis would result in a negative:
-        if(millis < 0)
-        {
-            //30.74522 - 31 = -1.74522
-            //Make millis have a zero in the first digit:
-            millis -= -1;
-
-            //Then take the first two decimal places by multiplying by 100:
-            millis *= 100f;
-
-            //Then round the millis:
-            millis = (float)Math.Truncate(millis);
-        }
-        //If the seconds got rounded down, then millis would result in 0.xxxxxxx:
-        else if(millis > 0) 
-        {
-            //Take the first two decimal places by multiplying by 100:
-            //0.12345 * 100 = 12.345
-            millis *= 100f;
-
-            //Then round the millis:
-            millis = (float)Math.Truncate(millis);
-        }
-        #endregion
-
-        #region Seconds
-
-        int minutesQuotient = (int)Math.Truncate(speedrunSeconds.Value / 60f);
-        int seconds = (int)Math.Truncate(speedrunSeconds.Value) - 60 * minutesQuotient;
-
-        #endregion
-
-        #region Minutes
-
-        int minutes = (int)Math.Truncate(speedrunSeconds.Value / 60f);
-
-        #endregion
-
-        text.text = minutes.ToString() + ":" + seconds.ToString() + ":" + millis.ToString();
+        SpeedrunTime speedrunTime = new SpeedrunTime(speedrunSeconds.Value);
+        text.text = speedrunTime.minutes + ":" + speedrunTime.seconds + ":" + speedrunTime.milliSeconds;
     }
 }
