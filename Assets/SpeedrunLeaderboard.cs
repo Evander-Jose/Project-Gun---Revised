4b775a79e0fb46e266aed48a865adce1cea677b8
Evander-Jose <jemandexistiert@gmail.com>
2021-05-29 17:32:27 +0700

    Fastest Speedrun Time Recorded
    Making use of PlayerPrefs, the fastest time for a speedrun through the four levels will be automatically recorded and saved.
    
    The fastest time could also be reset to its default value in the settings menu.

---
+++ Assets/SpeedrunLeaderboard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedrunLeaderboard : MonoBehaviour
+{
+    public FloatVariable fastestTime;
+    public FloatVariable currentFinishedTime;
+
+    private const string fastestTimeKey = "FASTEST_TIME";
+
+    public void SaveNewFastestTime()
+    {
+        bool currentIsFaster = currentFinishedTime.Value < fastestTime.Value && currentFinishedTime.Value > 0;
+        if(currentIsFaster)
+        {
+            PlayerPrefs.SetFloat(fastestTimeKey, currentFinishedTime.Value);
+            PlayerPrefs.Save();
+
+            fastestTime.Value = LoadSavedTime();
+        }
+    }
+
+    public void ResetFastestTime()
+    {
+        PlayerPrefs.SetFloat(fastestTimeKey, fastestTime.DefaultValue);
+        PlayerPrefs.Save();
+
+        fastestTime.Value = LoadSavedTime();     
+    }
+
+    public void UpdateFastestTime()
+    {
+        fastestTime.Value = LoadSavedTime();
+    }
+
+    private float LoadSavedTime()
+    {
+        if (PlayerPrefs.HasKey(fastestTimeKey))
+            return PlayerPrefs.GetFloat(fastestTimeKey);
+        else
+            return 0;
+    }
+}
